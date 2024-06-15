fa35e6d8a33461dbc1ea6490db3975f40adaf7c8
Chibueze Oti <otichibueze@gmail.com>
2024-06-15 22:03:49 +0100

    updates on gradle app running on mobile device

--- Secret Diary/Secret Diary/task/src/main/java/org/hyperskill/secretdiary/LoginActivity.kt
+++ Secret Diary/Secret Diary/task/src/main/java/org/hyperskill/secretdiary/LoginActivity.kt
@@ -1,7 +1,7 @@
 package org.hyperskill.secretdiary
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+//import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,7 +14,7 @@ const val PWD = 1234
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
